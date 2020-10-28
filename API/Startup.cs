af28319e69d25dedadb7e1002eb796527aa0b8a1
Miguel <miguellearning7@gmail.com>
2020-10-28 15:58:30 +0000

    End of Section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -29,7 +29,13 @@ namespace API
             services.AddApplicationServices();
 
             services.AddSwaggerDocumentation();
-
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
 
         }
 
@@ -43,7 +49,7 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
-
+            app.UseCors("CorsPolicy");
             app.UseAuthorization();
 
             app.UseSwaggerDocumentation();
