af5c1496b13a8827454f8389d8a1e6acefa25b78
Miguel <miguellearning7@gmail.com>
2020-11-02 11:16:06 +0000

    End of Section 16

--- API/Dtos/AddressDto.cs
+++ API/Dtos/AddressDto.cs
@@ -1,13 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class AddressDto
     {
-        
+        [Required]
         public string AddressLine1 { get; set; }
+        [Required]
         public string AddressLine2 { get; set; }
+        [Required]
         public string AddressLine3 { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string State { get; set; }
+        [Required]
         public string Zipcode { get; set; }
     }
 }
